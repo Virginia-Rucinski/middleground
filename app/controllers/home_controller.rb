57c9b8f6868ab2502097cc12e25ceabbd921fafc
Camille Davis <camilleedavis@gmail.com>
2016-08-04 15:38:01 -0400

    wolfram alpha api

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
   	@googlemaps_api_key = Rails.application.secrets.googlemaps_api_key
+    @wolframalpha_api_key = Rails.application.secrets.wolframalpha_api_key
   end
 end
