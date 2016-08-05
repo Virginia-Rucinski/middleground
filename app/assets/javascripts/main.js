da6880a05ef68ac8b762064a562bfc5363f5d22c
Camille Davis <camilleedavis@gmail.com>
2016-08-05 14:11:08 -0400

    toggle action added to header

---
+++ app/assets/javascripts/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+  $('.accordion').on('click', '.accordion-control', function(e){
+    e.preventDefault();
+    $(this)
+      .next('.accordion-panel')
+      .not(':animated')
+      .slideToggle();
+  })
+});
