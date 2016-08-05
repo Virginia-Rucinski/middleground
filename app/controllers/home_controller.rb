8def4c56d7f4c72c8396bd48a7365f128a2d628b
Camille Davis <camilleedavis@gmail.com>
2016-08-05 11:07:24 -0400

    removed Wolfram Alpha

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
   	@googlemaps_api_key = Rails.application.secrets.googlemaps_api_key
-    @wolframalpha_api_key = Rails.application.secrets.wolframalpha_api_key
-  end
+
+  end  
 
   def places
   end
