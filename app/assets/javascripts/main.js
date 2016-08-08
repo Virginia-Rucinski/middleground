08b0f51544d59e7ede831fdc22f079edbd0c80af
Camille Davis <camilleedavis@gmail.com>
2016-08-08 14:25:15 -0400

    toggling about info

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,9 +1,16 @@
 $(document).ready(function(){
-  $('.accordion').on('click', '.accordion-control', function(e){
-    e.preventDefault();
-    $(this)
-      .next('.accordion-panel')
-      .not(':animated')
-      .slideToggle();
-  })
-});
+ 
+ $('.accordion-control').on('click', function(){
+  $('#header').animate({height: "60vh"})
+    $('.accordion-panel').toggle('slow', function(){ $('.accordion-panel').addClass('.show');
+    });
+  }); 
+
+ $('.accordion-close').click(function(){
+     $('.accordion-panel').slideUp('slow', function(){
+      $('#header').animate({height: "30vh"})  
+      $('.accordion-panel').addClass('.hide')
+    });  
+  }); 
+
+}); //document end
